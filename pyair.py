ecd0cbcd19bff5470f2fc18d2d819d1dbb992b87
Abhizx10 <abhishere@gmail.com>
2018-06-25 17:48:54 -0500

    Added more streaming sources and realigned code

--- pyair.py
+++ pyair.py
@@ -1,12 +1,16 @@
 '''
 Python script to play internet radio station and podcasts in VLC
 '''
-import vlc
+
 import sys
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
+from urllib.request import urlopen
+import vlc
 
+'''
+Function to scrap the latest podcast url and title
+'''
 def getLatestEpisodeUrl(rssFeed,podcast):
   # Ignore SSL certificate errors
   ctx = ssl.create_default_context()
@@ -27,48 +31,110 @@ def getLatestEpisodeUrl(rssFeed,podcast):
 
     return(tag.get('url',None))
 
+'''
+Function to handle media player selection
+'''
 def play(id):
+  radio = False
   if id =='1':
       url = 'http://icecast.vrtcdn.be/stubru-high.mp3'
-  elif id =='2':
+      radio = True
+  elif id == '2':
+      url = 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_q'
+      radio = True
+  elif id =='3':
+      url = 'http://rfcmedia.streamguys1.com/MusicPulse.mp3'
+      radio = True
+  elif id == '4':
+      rssFeed = 'https://www.npr.org/rss/podcast.php?id=510019'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='10':
       rssFeed = 'http://feeds.gimletmedia.com/hearreplyall'
       url = getLatestEpisodeUrl(rssFeed,"R")
-  elif id =='3':
+  elif id =='11':
       rssFeed = 'http://feeds.feedburner.com/freakonomicsradio'
       url = getLatestEpisodeUrl(rssFeed,"F")
-  elif id =='4':
-    rssFeed = 'https://www.npr.org/rss/podcast.php?id=510289'
-    url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='12':
+      rssFeed = 'https://www.npr.org/rss/podcast.php?id=510289'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='15':
+      rssFeed = 'https://feeds.megaphone.fm/revisionisthistory'
+      url = getLatestEpisodeUrl(rssFeed,"R")
   elif id =='5':
-    rssFeed = 'https://feeds.megaphone.fm/revisionisthistory'
-    url = getLatestEpisodeUrl(rssFeed,"R")
-  else:
-    print('Invalid Input\n')
-    sys.exit()
+      rssFeed = 'https://podcasts.files.bbci.co.uk/b006qnmr.rss'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='13':
+      rssFeed = 'http://feeds.wnyc.org/radiolab'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='14':
+      rssFeed = 'http://feeds.99percentinvisible.org/99percentinvisible'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='16':
+      rssFeed = 'https://www.npr.org/rss/podcast.php?id=510318'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id =='6':
+      rssFeed = 'http://feed.songexploder.net/SongExploder'
+      url = getLatestEpisodeUrl(rssFeed,"R")
+  elif id == '17':
+      rssFeed = 'https://www.npr.org/rss/podcast.php?id=500005'
+      url = getLatestEpisodeUrl(rssFeed,"R")
 
+  else:
+    print('\nInvalid Input!\n')
+    mainMenu()
+  if(not(radio)):
+    choice = input("\nDo you want to play this episode?(y/n): ")
+    if(choice =='n'):
+      mainMenu()
   p = vlc.MediaPlayer(url)
   p.play()
 
   try:
     while True:
-      print("\nPlaying Audio Track,press p to pause, c to continue, Ctlr + C to stop\n")
+      print("\nPlaying Audio Track, press p to pause, c to continue, m to go back to main menu and Ctlr + C to stop\n")
       i = input("")
       if i=='p':
         p.pause()
       elif i=='c':
         p.play()
+      elif i == 'm':
+        p.stop()
+        mainMenu()
   except KeyboardInterrupt:
       print("Stopped playing the streaming station")
       p.stop()
       sys.exit()
+    
+'''
+Display the main menu
+'''
+def mainMenu():
+    try:
+        print('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('~~~~ Welcome to your media streaming ~~~~~')
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Enter the number to play the stream')
+        print('\nCategory: Music -\n')
+        print('1.  Studio Brussel')
+        print('2.  BBC Radio One')
+        print('3.  Tunein Today\'s Hits')
+        print('4.  All Songs Considered Podcast')
+        print('5.  Desert Island Discs')
+        print('6.  Song Exploder')
+        print('\nCategory: Curosity -\n')
+        print('10.  Reply All')
+        print('11.  Freakonomics')
+        print('12.  Planet Money')
+        print('13.  Radiolab')
+        print('14.  99 Percent Invisible')
+        print('15.  Revisionist History Podcast')
+        print('\nCategory: Current Affairs - \n')
+        print('16.  Up First')
+        print('17.  NPR News Now')
+        id = input("\nPress number to play : ")
+        play(id)
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == '__main__':
-    print('~~Welcome to your media streaming~~~')
-    print('Enter the number to play the stream')
-    print('1. Studio Brussel')
-    print('2. Play the latest Reply All Podcast')
-    print('3. Play the latest Freakonomics Podcast')
-    print('4. Play the latest Planet Money Podcast')
-    print('5. Play the latest Revisionist History Podcast')
-    id = input("Press number to play : ")
-    play(id)
+    mainMenu()
