8b4d064bc4975c56edb4ec7d783d1519aa9812c8
Abhizx10 <abhishere@gmail.com>
2018-06-25 20:08:12 -0500

    Improve readibility and follow coding guidelines

--- pyair.py
+++ pyair.py
@@ -1,85 +1,79 @@
-'''
-Python script to play internet radio station and podcasts in VLC
-'''
+""" Python script to play internet radio station and podcasts in VLC """
 
 import sys
-from bs4 import BeautifulSoup
 import ssl
+
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import vlc
 
-'''
-Function to scrap the latest podcast url and title
-'''
-def getLatestEpisodeUrl(rssFeed,id):
-  # Ignore SSL certificate errors
-  ctx = ssl.create_default_context()
-  ctx.check_hostname = False
-  ctx.verify_mode = ssl.CERT_NONE
 
-  html = urlopen(rssFeed, context=ctx).read()
-  soup = BeautifulSoup(html, "html.parser")
+def get_latest_episode_url(rssFeed,id):
+    """ Function to scrap the latest podcast url and title"""
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
-  tags = soup('enclosure')
+    html = urlopen(rssFeed, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
 
-  for tag in tags:
-    # Look at the parts of a tag
-    title = soup.find('title')
-    if(not(id == "11")):
-      title = title.find_next('title')
+    tags = soup('enclosure')
+
+    for tag in tags:  # Look at the parts of a tag
+        title = soup.find('title')
+        if(not(id == "11")):
+            title = title.find_next('title')
     print("\nEpisode Title :",title.find_next('title').contents[0])
 
     return(tag.get('url',None))
 
-'''
-Function to handle media player selection
-'''
-def play(id):
-  isFound = False
-  radio = False
-  with open('links.txt') as f:
-      for line in f:
-          if line.startswith(id):
-              link = line.split()
-              url = link[2]
-              #print("#Debug url :"+url)
-              isFound = True
-              if(link[3]=='r'):
-                radio = True
-              else:
-                url = getLatestEpisodeUrl(url,id)
-                #print("#Debug  podcast link  :"+url)
-  if(not(isFound)):
-      print('\nInvalid Input!\n')
-      mainMenu()
 
-  if(not(radio)):
+def play(id):
+    """ Function to handle media player selection """
+    is_found = False                   # Is stream ID found? 
+    radio = False
+    with open('links.txt') as f:
+        for line in f:
+            if line.startswith(id):
+                link = line.split()
+                url = link[2]
+                is_found = True
+                if(link[3]=='r'):
+                  radio = True
+                else:
+                  url = get_latest_episode_url(url,id)
+    if(not(is_found)):
+        print('\nInvalid Input!\n')
+        main_menu()
+
+    if(not(radio)):
         choice = input("\nDo you want to play this episode?(y/n): ")
         if(choice =='n'):
             mainMenu()
-  p = vlc.MediaPlayer(url)
-  p.play()
+    p = vlc.MediaPlayer(url)
+    p.play()
 
-  try:
-    while True:
-      print("\nPlaying Audio Track, press p to pause, c to continue, m to go back to main menu and Ctlr + C to stop\n")
-      i = input("")
-      if i=='p':
-        p.pause()
-      elif i=='c':
-        p.play()
-      elif i == 'm':
+    try:
+        while True:
+            print("\nPlaying Audio Track, press p to pause, c to continue, m to go back to main menu and Ctlr + C to stop\n")
+            i = input("")
+            if i=='p':
+                p.pause()
+            elif i=='c':
+                p.play()
+            elif i == 'm':
+                p.stop()
+                mainMenu()
+    except KeyboardInterrupt:
+        print("Stopped playing the streaming station")
         p.stop()
-        mainMenu()
-  except KeyboardInterrupt:
-      print("Stopped playing the streaming station")
-      p.stop()
-      sys.exit()
+        sys.exit()
+
     
-'''
-Display the main menu
-'''
-def mainMenu():
+
+def main_menu():
+    """ Display the main menu """
     try:
         print('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print('~~~~ Welcome to your media streaming ~~~~~')
@@ -108,4 +102,4 @@ def mainMenu():
         sys.exit()
 
 if __name__ == '__main__':
-    mainMenu()
+    main_menu()
